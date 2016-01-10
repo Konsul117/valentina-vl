2e82ff75a4c8e94ffb8ccafbcaac805a54535254
Казанцев Александр <konsul117@bk.ru>
2016-01-10 21:02:13 +1000

    шапка, меню, reset css

--- frontend/views/layouts/blocks/header.php
+++ frontend/views/layouts/blocks/header.php
@@ -1,3 +1,7 @@
+<?php
+use yii\helpers\Html;
+?>
+
 <header>
 
 	<div class="last-photos">
@@ -29,13 +33,32 @@
 
 	</div>
 
-	<div class="img-name img-name-desktop"></div>
+	<div class="img-name img-name-desktop">
+		<?= Html::a('', ['/']) ?>
+	</div>
 
 	<div class="img-name img-name-mobile">
+		<?= Html::a('', ['/']) ?>
 		<div class="wrapper">
 			<div class="img-part left-part"></div>
 			<div class="img-part right-part"></div>
 		</div>
-
 	</div>
+
+	<div class="menu">
+		<ul class="menu-items">
+			<li class="item-biser">
+				<?= Html::a('', ['/']) ?>
+			</li>
+
+			<li class="item-not-biser">
+				<?= Html::a('', ['/']) ?>
+			</li>
+
+			<li class="item-contacts">
+				<?= Html::a('', ['/']) ?>
+			</li>
+		</ul>
+	</div>
+
 </header>
