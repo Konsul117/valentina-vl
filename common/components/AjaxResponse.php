df348ed1402fd59fc251c7dbd16673dbc105f2df
Казанцев Александр <konsul117@bk.ru>
2016-01-16 21:35:52 +1000

    Недозалитый файл

---
+++ common/components/AjaxResponse.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace common\components;
+
+class AjaxResponse {
+
+	/**
+	 * Данные
+	 * @var array
+	 */
+	public $data;
+
+	/**
+	 * Успешность запроса
+	 * @var boolean
+	 */
+	public $success;
+
+	/**
+	 * Ошибки
+	 * @var string[]
+	 */
+	public $errors = [];
+
+	/**
+	 * Html-контент
+	 * @var string
+	 */
+	public $html;
+
+
+}
