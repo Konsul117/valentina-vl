abccb9da1c5f2537d70d5606fc93bd29ad54aa72
Казанцев Александр <konsul117@bk.ru>
2016-02-23 03:56:41 +1000

    авторесайз для редактора - уточнить пределы авто высоты

--- backend/modules/editor/assets/js/tinymce_init.js
+++ backend/modules/editor/assets/js/tinymce_init.js
@@ -19,8 +19,10 @@ function tinymce_init(textarea_id, inner_css) {
 		plugins: [
 			'advlist autolink lists link image charmap print preview anchor',
 			'searchreplace visualblocks code fullscreen',
-			'insertdatetime media table contextmenu paste code'
+			'insertdatetime media table contextmenu paste code',
+			'autoresize'
 		],
+		autoresize_max_height: 500,
 		toolbar: 'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image',
 		content_css: [
 			'//fast.fonts.net/cssapi/e6dc9b99-64fe-4292-ad98-6974f93cd2a2.css',
