f2cf3ffb1495c8d616f2237198eb0dcfb676c036
Казанцев Александр <konsul117@bk.ru>
2016-10-16 05:07:16 +1000

    правка viewport

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -14,8 +14,9 @@ CommonAsset::register($this);
 <!DOCTYPE html>
 <html lang="<?= Yii::$app->language ?>">
 <head>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
 	<meta charset="<?= Yii::$app->charset ?>">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
 
 	<?= Html::csrfMetaTags() ?>
 
